e34d542b9445b2bcc93255563d7820b2afc25eea
0xLucqs <lucaslevy10@gmail.com>
2026-07-16 17:48:10 +0200

    chore: ignore local workspace files

--- scratchpad/sha-sumcheck-spike/target/release/deps/spike-f7a750de0b9881c9.d
+++
@@ -1,9 +0,0 @@
-/Users/lucas/eu-id/scratchpad/sha-sumcheck-spike/target/release/deps/spike-f7a750de0b9881c9.d: src/main.rs src/circuit.rs src/field.rs src/simd.rs src/sumcheck.rs
-
-/Users/lucas/eu-id/scratchpad/sha-sumcheck-spike/target/release/deps/spike-f7a750de0b9881c9: src/main.rs src/circuit.rs src/field.rs src/simd.rs src/sumcheck.rs
-
-src/main.rs:
-src/circuit.rs:
-src/field.rs:
-src/simd.rs:
-src/sumcheck.rs:
