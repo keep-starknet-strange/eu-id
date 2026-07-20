38db76e8d9295d61c9824c0f971047e0a441acb6
micbakos <micbakos@gmail.com>
2026-07-20 16:31:24 +0300

    Create unified interface for ML-DSA and P256

--- crates/sdk/src/mapping.rs
+++ crates/sdk/src/mapping.rs
@@ -4,7 +4,7 @@ use std::collections::HashSet;
 
 use eu_id_prover::{all_nationality_codes, Date, Policy};
 
-use crate::{PredicateMode, ZkError, ZkPublicStatement};
+use crate::{IssuerKey, PredicateMode, ZkError, ZkPublicStatement};
 
 fn invalid(msg: impl Into<String>) -> ZkError {
     ZkError::InvalidInput(msg.into())
@@ -154,7 +154,7 @@ mod tests {
             version: 1,
             doctype: "eu.europa.ec.eudi.pid.1".to_string(),
             namespace: "eu.europa.ec.eudi.pid.1".to_string(),
-            issuer_public_key_hash: vec![0x11; 32],
+            issuer_key: IssuerKey::MlDsa { pk_hash: vec![0x11; 32] },
             today_epoch_day: days_from_civil(2020, 1, 1) as i32,
             nonce: vec![0xab, 0xcd],
             predicate_mode: mode,
